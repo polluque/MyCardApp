220c318748f570753abe00dfb003a9e399bacb15
polluque <polluque26081997@gmail.com>
2022-10-20 15:38:59 -0500

    mycardapp_v1.0

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp()); //todo metodo termina en ;
+
+}
+// widget sin estado, todo lo visto en pantalla no sufre modificaciones 
+// toda la siguiente clase soporta clases sin estdo 
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.indigo,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+                radius: 60.0, //valor numerico,siempre decimal
+                backgroundColor: Colors.white,
+                backgroundImage: NetworkImage(
+                  "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg"
+                ), 
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+// de aqui para arriba es la estrcutura basica de la interfaz 
+ // widget en sentido vertical (colum)
